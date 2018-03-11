9b28d96765c414808d2068638acf85faa71f1413
Kerry Burns <kerryalanburns@gmail.com>
2018-03-10 20:57:15 -0600

    created class and array

--- Weekend_Salary_Calc.js
+++ Weekend_Salary_Calc.js
@@ -2,14 +2,37 @@
 $(document).ready(readyNow);
 
 function readyNow() {
-appendDom();
-// runEventListeners();
-//   $('#submit').on('click', someFunction);
-// //this will be where we put our mouse clicks - ref color blocks assignment
-// }
+addClickHandlers();
 
-function appendDom() {
-console.log('in appendDom');
 
-} //end appendDom
 } //end readyNow
+
+function addClickHandlers() {
+$('#submit').on('click', addEmployeeInfo);
+} //end addClickHandlers
+
+function addEmployeeInfo() {
+  console.log('in addEmployeeInfo');
+  let whatTheUserTyped = ($('#firstNameIn').val(), $('#lastNameIn').val());
+console.log('the user typed:', whatTheUserTyped);
+$('.container').append('<td>' + whatTheUserTyped + '<td>');
+
+} //end addEmployeeInfo
+
+
+class Employee {
+  constructor( first, last, id, title, salary ) {
+    this.first = first;
+    this.last = last;
+    this.iD = id;
+    this.title = title;
+    this.salary = salary;
+  } //end constructor
+} //end Employee Class
+
+let employees = [];
+
+function newEmployee( first, last, id, title, salary ) {
+console.log('in newEmployee:', first, last, id, title, salary );
+
+}
